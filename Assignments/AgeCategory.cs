4c651c4368da87f249937f7aab96055fc7028dc5
Umarrrrrrrrrrrrrrrr <np05cp4a230043@iic.edu.np>
2025-11-11 07:54:19 +0545

    number 3 question answer

---
+++ Assignments/AgeCategory.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class AgeCategory
+{
+    public void CheckAge()
+    {
+        Console.Write("Enter your age");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 13)
+        {
+            Console.WriteLine("You are a child");
+        }
+        else if (age >= 13 && age <= 19)
+        {
+            Console.WriteLine("You are a teenager");
+        }
+        else if (age >= 20)
+        {
+            Console.WriteLine("You are an adult");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input");
+        }
+    }
+}
